8f2758f9fd1a6a3fde130cbfd2d846d627833124
AlishaKausheen <alishakausheen@gmail.com>
2023-11-19 06:58:59 -0800

    js

---
+++ Client/client.js
@@ -0,0 +1,22 @@
+const joinRoomButton = document.getElementById(`room-button`);
+const messageInput = document.getElementById(`message-input`);
+const roomInput = document.getElementById(`room-input`);
+const form = document.getElementById(`form`);
+const sendBtn = document.getElementById(`send-button`)
+
+sendBtn.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const message = messageInput.value;
+    const room = roomInput.value;
+    if (message === ``) { return displayMessage(message); }
+
+    messageInput.value = ``;
+})
+joinRoomButton.addEventListener(`click`, () => {
+    const room= roomInput.value
+})
+function displayMessage(message) {
+    const div = document.createElement(`div`)
+    div.textContent = message;
+    document.getElementById(`message-container`).append(div)
+}
